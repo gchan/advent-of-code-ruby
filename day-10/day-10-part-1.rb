8775fed09a60ff300e2ef57dac98a3848b125a0c
Gordon Chan <developer.gordon+github@gmail.com>
2015-12-10 19:28:22 +1300

    Day 10

---
+++ day-10/day-10-part-1.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-10-input.txt", __FILE__)
+input     = File.read(file_path)
+
+numbers = input.chars.map(&:to_i)
+
+40.times do
+  numbers = numbers.slice_when { |before, after| before != after }.
+    flat_map { |numbers| [numbers.count, numbers.first] }
+end
+
+puts numbers.length
