5476e3949ff4d7caba5db7833ab309f21426c2ad
Gordon Chan <developer.gordon+github@gmail.com>
2015-12-11 21:33:13 +1300

    Day 11 - Use String#succ

--- day-11/day-11-part-1.rb
+++ day-11/day-11-part-1.rb
@@ -3,36 +3,24 @@
 file_path = File.expand_path("../day-11-input.txt", __FILE__)
 password  = File.read(file_path)
 
-def increasing_straight?(string)
-  string.chars.each_cons(3).any? do |a, b, c|
+def increasing_straight?(password)
+  password.chars.each_cons(3).any? do |a, b, c|
     a.ord + 1 == b.ord && a.ord + 2 == c.ord
   end
 end
 
-def two_pairs?(string)
-  string.scan(/(.)\1/).count >= 2
+def two_pairs?(password)
+  password.scan(/(.)\1/).count >= 2
 end
 
-def valid_password?(string)
-  increasing_straight?(string) &&
-    two_pairs?(string)
+def valid_password?(password)
+  increasing_straight?(password) && two_pairs?(password)
 end
 
-def increment_password(characters, index)
-  characters[index] = ((characters[index].ord - 96) % 26 + 97).chr
-  new_char = characters[index]
-
-  increment_password(characters, index) if %w(i o l).include?(new_char)
-  increment_password(characters, index - 1) if new_char == 'a'
-end
-
-characters    = password.chars
 next_password = 1
 
 while next_password != 0
-  increment_password(characters, -1)
-
-  password = characters.join
+  password.succ!
 
   next_password -= 1 if valid_password?(password)
 end
