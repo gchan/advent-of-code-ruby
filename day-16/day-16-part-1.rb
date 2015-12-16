3ba19a525714db6cbdc24401f9cfdc6c04fc96aa
Gordon Chan <developer.gordon+github@gmail.com>
2015-12-16 18:41:53 +1300

    Day 16

---
+++ day-16/day-16-part-1.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-16-input.txt", __FILE__)
+sues      = File.readlines(file_path)
+
+sues.map! do |sue|
+  {
+    number:      sue.scan(/Sue (\d+)/).flatten.map(&:to_i).first,
+    children:    sue.scan(/children: (\d+)/).flatten.map(&:to_i).first,
+    cats:        sue.scan(/cats: (\d+)/).flatten.map(&:to_i).first,
+    samoyeds:    sue.scan(/samoyeds: (\d+)/).flatten.map(&:to_i).first,
+    pomeranians: sue.scan(/pomeranians: (\d+)/).flatten.map(&:to_i).first,
+    akitas:      sue.scan(/akitas: (\d+)/).flatten.map(&:to_i).first,
+    vizslas:     sue.scan(/vizslas: (\d+)/).flatten.map(&:to_i).first,
+    goldfish:    sue.scan(/goldfish: (\d+)/).flatten.map(&:to_i).first,
+    trees:       sue.scan(/trees: (\d+)/).flatten.map(&:to_i).first,
+    cars:        sue.scan(/cars: (\d+)/).flatten.map(&:to_i).first,
+    perfumes:    sue.scan(/perfumes: (\d+)/).flatten.map(&:to_i).first
+  }
+end
+
+compounds = {
+  children: 3,
+  cats: 7,
+  samoyeds: 2,
+  pomeranians: 3,
+  akitas: 0,
+  vizslas: 0,
+  goldfish: 5,
+  trees: 3,
+  cars: 2,
+  perfumes: 1
+}
+
+real_sue = sues.find do |sue|
+  compounds.all? do |compound, value|
+    sue[compound].nil? || sue[compound] == value
+  end
+end
+
+puts real_sue[:number]
