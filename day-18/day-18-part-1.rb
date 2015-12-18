69f6d4979b92841b08656bdc2c774379291c785e
Gordon Chan <developer.gordon+github@gmail.com>
2015-12-18 22:49:52 +1300

    Day 18

---
+++ day-18/day-18-part-1.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+file_path  = File.expand_path("../day-18-input.txt", __FILE__)
+input      = File.readlines(file_path)
+
+grid = Array.new(100) { Array.new(100, false) }
+
+input.each_with_index do |input, y|
+  input.strip.chars.each_with_index do |state, x|
+    grid[y][x] = state == '#'
+  end
+end
+
+def neighbours_on(grid, x, y)
+  x_coords = [x - 1, x, x + 1].select { |x| x.between?(0, 99) }
+  y_coords = [y - 1, y, y + 1].select { |y| y.between?(0, 99) }
+
+  x_coords.product(y_coords).
+    reject { |coords| coords == [x, y] }.
+    count { |x, y| grid[y][x] }
+end
+
+100.times do
+  new_grid = Array.new(100) { Array.new(100, false) }
+
+  (0..99).to_a.product((0..99).to_a).each do |x, y|
+    neighbours = neighbours_on(grid, x, y)
+
+    if grid[y][x]
+      new_grid[y][x] = neighbours.between?(2, 3)
+    else
+      new_grid[y][x] = neighbours == 3
+    end
+  end
+
+  grid = new_grid
+end
+
+puts grid.flatten.count { |state| state }
