989ff4678b6d59f33876cb932852ede60b7a31f4
Gordon Chan <developer.gordon+github@gmail.com>
2016-12-01 17:34:15 +1300

    Update create_files.sh to support 2016's Advent of Code

--- create_files.sh
+++ create_files.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for i in `seq -w 1 25`; do
-  mkdir day-$i
-  touch day-$i/README.md
-  touch day-$i/day-$i-part-1.rb
-  touch day-$i/day-$i-part-2.rb
-  touch day-$i/day-$i-input.txt
-  chmod +x day-$i/*.rb
+for y in `seq -w 2015 2016`; do
+  mkdir $y
+
+  for i in `seq -w 1 25`; do
+    mkdir $y/day-$i
+    touch $y/day-$i/README.md
+    touch $y/day-$i/day-$i-part-1.rb
+    touch $y/day-$i/day-$i-part-2.rb
+    touch $y/day-$i/day-$i-input.txt
+    chmod +x $y/day-$i/*.rb
+  done
 done
