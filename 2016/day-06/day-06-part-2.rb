5df7ac944af4363ae83093e7c091eeea4a8c40ce
Gordon Chan <developer.gordon+github@gmail.com>
2016-12-06 18:26:07 +1300

    Day 6 2016

---
+++ 2016/day-06/day-06-part-2.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-06-input.txt", __FILE__)
+input     = File.read(file_path)
+
+lines = input.split("\n")
+
+message = Array.new(lines.first.length) {
+  Hash.new { |h, k| h[k] = 0 }
+}
+
+lines.each do |line|
+  line.chars.each.with_index do |char, index|
+    message[index][char] += 1
+  end
+end
+
+puts message.map { |chars|
+  chars.sort_by { |char, count| count }.first.first
+}.join
