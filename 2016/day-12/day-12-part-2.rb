e8db1df3538a6b9b364ca39df623b5360ec8f333
Gordon Chan <developer.gordon+github@gmail.com>
2016-12-13 23:20:51 +1300

    Day 12 2016

---
+++ 2016/day-12/day-12-part-2.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-12-input.txt", __FILE__)
+input     = File.read(file_path)
+
+instructions = input.split("\n")
+
+reg = {
+  "a" => 0,
+  "b" => 0,
+  "c" => 1,
+  "d" => 0
+}
+
+i = 0
+
+while i < instructions.length
+  cmd, arg1, arg2 = instructions[i].split(" ")
+
+  if arg1 =~ /\d/
+    val = arg1.to_i
+  else
+    val = reg[arg1]
+  end
+
+  case cmd
+  when 'cpy'
+    reg[arg2] = val
+  when 'jnz'
+    if val != 0
+      i += arg2.to_i
+      next
+    end
+  when 'inc'
+    reg[arg1] += 1
+  when 'dec'
+    reg[arg1] -= 1
+  end
+
+  i += 1
+end
+
+puts reg['a']
