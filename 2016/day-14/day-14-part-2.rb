51059b3f62cab7980a192125525c8032b5aa0c6b
Gordon Chan <developer.gordon+github@gmail.com>
2016-12-14 19:47:58 +1300

    Day 14 2016

---
+++ 2016/day-14/day-14-part-2.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+require 'set'
+
+file_path = File.expand_path("../day-14-input.txt", __FILE__)
+input     = File.read(file_path)
+
+triples = []
+keys = Set.new
+
+number = 0
+
+while keys.length < 64
+  digest = Digest::MD5.hexdigest("#{input}#{number}")
+
+  2016.times do
+    digest = Digest::MD5.hexdigest(digest)
+  end
+
+  five_match = digest =~ /(.)\1\1\1\1/
+  if five_match
+    triples.each do |triple, pattern|
+      if digest[five_match] == pattern[0] && triple + 1000 >= number
+        keys.add(triple)
+        puts triple if keys.count == 64
+        puts [keys.count, triple].join(", ")
+      end
+    end
+  end
+
+  triple_match = digest =~ /(.)\1\1/
+  triples << [number, digest[triple_match]] if triple_match
+
+  number += 1
+end
