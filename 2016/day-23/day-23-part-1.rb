57f45c68b99be8a49b611fd7267fb04b177aba67
Gordon Chan <developer.gordon+github@gmail.com>
2016-12-25 00:00:51 +1300

    Day 23 2016

---
+++ 2016/day-23/day-23-part-1.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-23-input.txt", __FILE__)
+input     = File.read(file_path)
+
+instructions = input.split("\n")
+
+reg = { "a" => 7 }
+
+i = 0
+
+def argToVal(arg, reg)
+  if arg =~ /\d/
+    arg.to_i
+  else
+    reg[arg]
+  end
+end
+
+while i < instructions.length
+  cmd, arg1, arg2 = instructions[i].split(" ")
+
+  arg1Val = argToVal(arg1, reg)
+  arg2Val = argToVal(arg2, reg)
+
+  case cmd
+  when 'cpy'
+    reg[arg2] = arg1Val
+  when 'jnz'
+    i += arg2Val - 1 if arg1Val != 0
+  when 'inc'
+    reg[arg1] += 1
+  when 'dec'
+    reg[arg1] -= 1
+  when 'tgl'
+    idx = i + reg[arg1]
+
+    if instructions[idx]
+      cmd, arg1, arg2 = instructions[idx].split(" ")
+
+      if arg2
+        replacement = cmd == "jnz" ? "cpy" : "jnz"
+      else
+        replacement = cmd == "inc" ? "dec" : "inc"
+      end
+
+      instructions[idx][0..2] = replacement
+    end
+  end
+
+  i += 1
+end
+
+puts reg['a']
