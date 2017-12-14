105e0b2277e7930b29b99d72325ddf855082acb0
Gordon Chan <developer.gordon+github@gmail.com>
2017-12-14 21:11:08 +1300

    Update create_files.sh to be directory independent

--- create_files.sh
+++ create_files.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DIR=$(dirname $(readlink -f $0))
+
 for y in `seq -w 2015 2017`; do
-  mkdir $y
+  mkdir $DIR/$y
 
   for i in `seq -w 1 25`; do
-    mkdir $y/day-$i
-    touch $y/day-$i/README.md
-    touch $y/day-$i/day-$i-part-1.rb
-    touch $y/day-$i/day-$i-part-2.rb
-    touch $y/day-$i/day-$i-input.txt
+    mkdir $DIR/$y/day-$i
+    touch $DIR/$y/day-$i/README.md
+    touch $DIR/$y/day-$i/day-$i-part-1.rb
+    touch $DIR/$y/day-$i/day-$i-part-2.rb
+    touch $DIR/$y/day-$i/day-$i-input.txt
     chmod +x $y/day-$i/*.rb
   done
 done
