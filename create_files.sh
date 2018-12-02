a751c2d4276488627f1b6d510c96963649670389
Gordon Chan <developer.gordon+github@gmail.com>
2018-12-02 22:37:49 +1300

    Update create_files.sh for 2018 and not create part 2 for day 25

--- create_files.sh
+++ create_files.sh
@@ -2,14 +2,17 @@
 
 DIR=$(dirname $(readlink -f $0))
 
-for y in `seq -w 2015 2017`; do
+for y in `seq -w 2015 2018`; do
   mkdir $DIR/$y
 
   for i in `seq -w 1 25`; do
     mkdir $DIR/$y/day-$i
-    touch $DIR/$y/day-$i/README.md
+    touch $DIR/$y/day-$i/README.txt
     touch $DIR/$y/day-$i/day-$i-part-1.rb
-    touch $DIR/$y/day-$i/day-$i-part-2.rb
+    if [ $i -ne '25' ]
+    then
+      touch $DIR/$y/day-$i/day-$i-part-2.rb
+    fi
     touch $DIR/$y/day-$i/day-$i-input.txt
     chmod +x $y/day-$i/*.rb
   done
