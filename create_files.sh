6d4a8abd0543f92738d6c86a4e1879f963d3ced6
Gordon Chan <developer.gordon+github@gmail.com>
2022-12-13 19:30:51 +1300

    Update file generator to use new template file

--- create_files.sh
+++ create_files.sh
@@ -2,18 +2,24 @@
 
 DIR=$(dirname $(readlink -f $0))
 
-for y in `seq -w 2021 2021`; do
+for y in `seq -w 2015 2022`; do
   mkdir $DIR/$y
 
   for i in `seq -w 1 25`; do
     mkdir $DIR/$y/day-$i
-    touch $DIR/$y/day-$i/README.txt
-    touch $DIR/$y/day-$i/day-$i-part-1.rb
-    if [ $i -ne '25' ]
-    then
-      touch $DIR/$y/day-$i/day-$i-part-2.rb
+
+    if [ -z "$(ls -A $DIR/$y/day-$i)" ]; then
+      cp template.rb $DIR/$y/day-$i/day-$i-part-1.rb
+
+      if [ $i -ne '25' ]
+      then
+        cp template.rb $DIR/$y/day-$i/day-$i-part-2.rb
+      fi
+      sed -i "s/\\$/${i}/g" $DIR/$y/day-$i/*.rb
+
+      touch $DIR/$y/day-$i/README.txt
+      touch $DIR/$y/day-$i/day-$i-input.txt
+      chmod +x $y/day-$i/*.rb
     fi
-    touch $DIR/$y/day-$i/day-$i-input.txt
-    chmod +x $y/day-$i/*.rb
   done
 done
---
+++ template.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-$-input.txt", __FILE__)
+input     = File.read(file_path)
