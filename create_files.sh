f69e10f14e6a2ae98c2edcea991d164317c3f769
Gordon Chan <developer.gordon+github@gmail.com>
2025-12-02 21:14:26 +1300

    Update create_files.sh for 2025 and 12 days

--- create_files.sh
+++ create_files.sh
@@ -2,16 +2,18 @@
 
 DIR=$(dirname $(readlink -f $0))
 
-for y in $(seq -w 2015 2024); do
+DAYS=12
+
+for y in $(seq -w 2015 2025); do
   mkdir $DIR/$y
 
-  for i in $(seq -w 1 25); do
+  for i in $(seq -w 1 $DAYS); do
     mkdir $DIR/$y/day-$i
 
     if [ -z "$(ls -A $DIR/$y/day-$i)" ]; then
       cp template.rb $DIR/$y/day-$i/day-$i-part-1.rb
 
-      if [ $i -ne '25' ]; then
+      if [ $i -ne $DAYS ]; then
         cp template.rb $DIR/$y/day-$i/day-$i-part-2.rb
       fi
       sed -i "" "s/\\$/${i}/g" $DIR/$y/day-$i/*.rb
